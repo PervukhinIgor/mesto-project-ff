c9f931aa2bf67df73dd466a0fa869a639847e3b6
Igor Pervukhin <pervukhin_ia@mail.ru>
2025-03-15 00:11:30 +0300

    create and delete

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,44 @@
-// @todo: Темплейт карточки
+// Функция создания карточки
+function createCard(cardData, deleteCallback) {
+  // Находим шаблон карточки
+  const cardTemplate = document.querySelector('#card-template').content;
+  
+  // Клонируем содержимое шаблона
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  // Находим элементы внутри карточки, которые нужно заполнить данными
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+  // Заполняем элементы данными из cardData
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+  
+  // Добавляем обработчик события на кнопку удаления
+  deleteButton.addEventListener('click', function() {
+      deleteCallback(cardElement);
+  });
+  
+  // Возвращаем готовый элемент карточки
+  return cardElement;
+}
 
-// @todo: DOM узлы
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
-// @todo: Функция создания карточки
+// Вывести карточки на страницу
 
-// @todo: Функция удаления карточки
+// Контейнер для карточек
+const placesList = document.querySelector('.places__list');
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function(cardData) {
+  // Создаем элемент карточки
+  const newCard = createCard(cardData, deleteCard);
+  
+  // Добавляем карточку в контейнер
+  placesList.appendChild(newCard);
+});
